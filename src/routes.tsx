1bb288fb6ded9578538470c4d2238cfc24ba953f
Adones Guerreiro <adoneslori@gmail.com>
2026-03-26 20:51:01 -0400

    refactor(routes): update route structure and import new page components

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,109 +3,191 @@ import { AppLayout } from "./AppLayout";
 import LoginPage from "./features/auth/pages/Login";
 import ForgotPasswordPage from "./features/auth/pages/ForgotPassword";
 import ResetPasswordPage from "./features/auth/pages/ResetPassword";
-import BookingPage from "./features/booking/pages";
 import DashboardPage from "./features/dashboard/pages";
-import UserPage from "./features/users/page";
-import CompanyPage from "./features/company/pages";
-import ServicePage from "./features/services/pages";
-import CustomerPage from "./features/customer/pages";
-import SchedulePage from "./features/schedule/pages";
-import AvaliableTimePage from "./features/avaliableTime/pages";
-import UnavaliableTimePage from "./features/unavaliableTime/pages";
+import UserPage from "./features/users/page/UserFormPage";
+import ServiceListPage from "./features/services/pages/ServiceListPage";
+import ServiceFormPage from "./features/services/pages/ServiceFormPage";
+import CompanyFormPage from "./features/company/pages/CompanyFormPage";
+import AvaliableTimeFormPage from "./features/avaliableTime/pages/AvaliableTimeFormPage";
+import AvaliableTimeListPage from "./features/avaliableTime/pages/AvaliableTimeListPage";
+import UnavailableTimeListPage from "./features/unavaliableTime/pages/UnavaliableTimeListPage/page";
+import UnavailableTimeFormPage from "./features/unavaliableTime/pages/UnavaliableTimeFormPage/page";
+import CustomerListPage from "./features/customer/pages/CustomerListPage";
+import CustomerFormPage from "./features/customer/pages/CustomerFormPage";
+import BookingFormPage from "./features/booking/pages/BookingFormPage";
+import ProtectedRoute from "./protectedroute";
+import ScheduleListPage from "./features/schedule/pages/ScheduleListPage";
+import ScheduleFormPage from "./features/schedule/pages/ScheduleFormPage";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/login",
-        element: <LoginPage />,
-      },
-      {
-        path: "/forgot-password",
-        element: <ForgotPasswordPage />,
-      },
-      {
-        path: "/reset-password",
-        element: <ResetPasswordPage />,
-      },
-      {
-        path: "/:slugCompany",
-        element: <BookingPage />,
-      },
-      {
-        path: "/",
-        element: <DashboardPage />,
-      },
-      {
-        path: "/user",
-        element: <UserPage />,
-      },
-      {
-        path: "/company",
-        element: <CompanyPage />,
-      },
-      {
-        path: "/service",
-        element: <ServicePage />,
-      },
-      {
-        path: "/service/new",
-        element: <ServicePage />,
-      },
-      {
-        path: "/service/:id",
-        element: <ServicePage />,
-      },
-      {
-        path: "/customer",
-        element: <CustomerPage />,
-      },
-      {
-        path: "/customer/new",
-        element: <CustomerPage />,
-      },
-      {
-        path: "/customer/:id",
-        element: <CustomerPage />,
-      },
-      {
-        path: "/schedule",
-        element: <SchedulePage />,
-      },
-      {
-        path: "/schedule/new",
-        element: <SchedulePage />,
-      },
-      {
-        path: "/schedule/:id",
-        element: <SchedulePage />,
-      },
-      {
-        path: "/avaliable-time",
-        element: <AvaliableTimePage/>,
-      },
-      {
-        path: "/avaliable-time/new",
-        element: <AvaliableTimePage/>,
-      },
-      {
-        path: "/avaliable-time/:id",
-        element: <AvaliableTimePage/>,
-      },
-      {
-        path: "/unavaliable-time",
-        element: <UnavaliableTimePage/>,
-      },
-      {
-        path: "/unavaliable-time/new",
-        element: <UnavaliableTimePage/>,
-      },
-      {
-        path: "/unavaliable-time/:id",
-        element: <UnavaliableTimePage/>,
-      },
-    ],
-  },
+	{
+		children: [
+			{
+				path: "/login",
+				element: <LoginPage />,
+			},
+			{
+				path: "/forgot-password",
+				element: <ForgotPasswordPage />,
+			},
+			{
+				path: "/reset-password",
+				element: <ResetPasswordPage />,
+			},
+			{
+				path: "/:slugCompany",
+				element: <BookingFormPage />,
+			},
+		],
+	},
+	{
+		path: "/",
+		element: <AppLayout />,
+		children: [
+			{
+				path: "/",
+				element: (
+					<ProtectedRoute>
+						<DashboardPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/user",
+				element: (
+					<ProtectedRoute>
+						<UserPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/company",
+				element: (
+					<ProtectedRoute>
+						<CompanyFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/service",
+				element: (
+					<ProtectedRoute>
+						<ServiceListPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/service/new",
+				element: (
+					<ProtectedRoute>
+						<ServiceFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/service/:id",
+				element: (
+					<ProtectedRoute>
+						<ServiceFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/customer",
+				element: (
+					<ProtectedRoute>
+						<CustomerListPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/customer/new",
+				element: (
+					<ProtectedRoute>
+						<CustomerFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/customer/:id",
+				element: (
+					<ProtectedRoute>
+						<CustomerFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/schedule",
+				element: (
+					<ProtectedRoute>
+						<ScheduleListPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/schedule/new",
+				element: (
+					<ProtectedRoute>
+						<ScheduleFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/schedule/:id",
+				element: (
+					<ProtectedRoute>
+						<ScheduleFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/avaliable-time",
+				element: (
+					<ProtectedRoute>
+						<AvaliableTimeListPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/avaliable-time/new",
+				element: (
+					<ProtectedRoute>
+						<AvaliableTimeFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/avaliable-time/:id",
+				element: (
+					<ProtectedRoute>
+						<AvaliableTimeFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/unavaliable-time",
+				element: (
+					<ProtectedRoute>
+						<UnavailableTimeListPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/unavaliable-time/new",
+				element: (
+					<ProtectedRoute>
+						<UnavailableTimeFormPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/unavaliable-time/:id",
+				element: (
+					<ProtectedRoute>
+						<UnavailableTimeFormPage />
+					</ProtectedRoute>
+				),
+			},
+		],
+	},
 ]);
